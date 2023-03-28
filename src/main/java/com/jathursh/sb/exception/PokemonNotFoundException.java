37d5e72781e56a4918523f128243da710077650e
Justy-11 <jathursh11@gmail.com>
2023-03-28 12:37:19 +0530

    adding custom exception PokemonNotFoundException

--- src/main/java/com/jathursh/sb/exception/PokemonNotFoundException.java
+++ src/main/java/com/jathursh/sb/exception/PokemonNotFoundException.java
@@ -1,9 +1,5 @@
 package com.jathursh.sb.exception;
 
-import lombok.Data;
-
 public class PokemonNotFoundException extends RuntimeException{
-    private int statusCode;
-    private String message;
-    private Data timestamp;
+
 }
