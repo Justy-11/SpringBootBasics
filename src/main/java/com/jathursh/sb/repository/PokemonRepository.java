f4baa1359c26abd6e890ec39b6bca581e1e9d923
Justy-11 <jathursh11@gmail.com>
2023-03-30 12:06:20 +0530

    + Custom query method in PokemonRepo

--- src/main/java/com/jathursh/sb/repository/PokemonRepository.java
+++ src/main/java/com/jathursh/sb/repository/PokemonRepository.java
@@ -3,7 +3,10 @@ package com.jathursh.sb.repository;
 import com.jathursh.sb.model.Pokemon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer> {
 
+    Optional<Pokemon> findByType(String type);
 
 }
